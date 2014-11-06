0f533a3a85260c507e65af309fff1954621ca9f0
Mobile Making <mm@Alexs-MacBook-Pro-2.local>
2014-11-05 17:48:10 -0800

    Initial Commit

---
+++ Za Hunter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Za Hunter
+//
+//  Created by Mobile Making on 11/5/14.
+//  Copyright (c) 2014 Alex Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Za Hunter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Za Hunter
+//
+//  Created by Mobile Making on 11/5/14.
+//  Copyright (c) 2014 Alex Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
